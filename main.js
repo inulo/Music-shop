c6a38dd20a66b5602d882dfad68f2da1d2a04583
V. S. Kanchev <27562113+inulo@users.noreply.github.com>
2021-09-27 19:32:57 +0100

    Update main.js

--- main.js
+++ main.js
@@ -6,27 +6,27 @@ const context = {
       image: 'https://content.codecademy.com/courses/learn-handlebars/musicon/electronic-keyboard.png',
       name: 'Teclado Eletrônico',
       description: 'Um piano de boas-vindas ao século XXI. Os pianistas celebram o formato compacto e a diversidade de ritmos sintetizados, tudo em uma máquina musical magistral.',
-      price: '$1,999.00',
-      sale: '$1,699.89'
+      price: '€1,999.00',
+      sale: '€1,699.89'
     },
     {
       image: 'https://content.codecademy.com/courses/learn-handlebars/musicon/electric-guitar.png',
       name: 'Guitarra elétrica',
       description: 'Junte-se às lendas dos anos 50 e 60 quando o casamento do violão e da eletricidade criou o instrumento mais influente de uma geração. Nota: picaretas venderam separadamente.',
-      price: '$599.99'
+      price: '€599.99'
     },
     {
       image: 'https://content.codecademy.com/courses/learn-handlebars/musicon/bass-guitar.png',
       name: 'Baixo',
       description: 'Experimente a personificação de frequências funkadelic que é o baixo. Deixe as notas graves profundas do baixo ressoar com os batimentos cardíacos em todos os lugares.',
-      price: '$624.99'
+      price: '€624.99'
     },
     {
       image: 'https://content.codecademy.com/courses/learn-handlebars/musicon/drum-kit.png',
       name: 'Kit de bateria',
       description: 'Já pensou, "um instrumento não é suficiente?" Encontre uma resposta no kit de bateria. Coordene uma coleção de tambores e pratos para ditar o ritmo da obra-prima musical.',
-      price: '$649.00',
-      sale: '$349.00'
+      price: '€649.00',
+      sale: '€349.00'
     }
   ]
 };
