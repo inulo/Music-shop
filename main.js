efa7ad2db740571a0f9fc9334207d0e3cd66713d
V. S. Kanchev <27562113+inulo@users.noreply.github.com>
2021-09-27 18:47:31 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,32 +1,32 @@
 const context = {
-  title: 'Welcome to Musicon',
+  title: 'Bem-vindo ao Musicon',
   body: 'Musicon é uma loja musical em desenvolvimento com a missão de compartilhar a alegria da música. Essas magníficas ferramentas auditivas são projetadas com criadores musicais, como você, em mente. Hobbyists, iniciantes e especialistas podem apreciar os sons resplandecentes fornecidos por cada um dos instrumentos que carregamos. Junte-se a nós para entregar as vibrações eufóricas da melodia aos cidadãos do mundo!',
   instruments: [
     {
       image: 'https://content.codecademy.com/courses/learn-handlebars/musicon/electronic-keyboard.png',
-      name: 'Electronic Keyboard',
-      description: 'A piano welcomed to the 21th century. Pianists celebrate the compact form factor and the diversity of synthesized rhythms all in one masterful musical machine.',
-      price: '$1,999.00',
-      sale: '$1,699.89'
+      name: 'Teclado Eletrônico',
+      description: 'Um piano de boas-vindas ao século XXI. Os pianistas celebram o formato compacto e a diversidade de ritmos sintetizados, tudo em uma máquina musical magistral.',
+      preço: '$1,999.00',
+      promoção: '$1,699.89'
     },
     {
       image: 'https://content.codecademy.com/courses/learn-handlebars/musicon/electric-guitar.png',
-      name: 'Electric Guitar',
-      description: 'Join the legends of the \'50s and \'60s when the marriage of guitar and electricity created the most influential instrument of a generation. Note: picks sold separately.',
-      price: '$599.99'
+      name: 'Guitarra elétrica',
+      description: 'Junte-se às lendas dos anos 50 e 60 quando o casamento do violão e da eletricidade criou o instrumento mais influente de uma geração. Nota: picaretas venderam separadamente.',
+      preço: '$599.99'
     },
     {
       image: 'https://content.codecademy.com/courses/learn-handlebars/musicon/bass-guitar.png',
-      name: 'Bass Guitar',
-      description: 'Experience the embodiment of funkadelic frequencies that is the bass guitar. Let the deep low notes of the bass guitar resonate with heartbeats everywhere.',
-      price: '$624.99'
+      name: 'Baixo',
+      description: 'Experimente a personificação de frequências funkadelic que é o baixo. Deixe as notas graves profundas do baixo ressoar com os batimentos cardíacos em todos os lugares.',
+      preço: '$624.99'
     },
     {
       image: 'https://content.codecademy.com/courses/learn-handlebars/musicon/drum-kit.png',
-      name: 'Drum Kit',
-      description: 'Ever thought, "one instrument isn\'t enough?" Find an answer in the drum kit. Coordinate a collections of drums and cymbals to dictate the rhythm of musical masterpiece.',
-      price: '$649.00',
-      sale: '$349.00'
+      name: 'Kit de bateria',
+      description: 'Já pensou, "um instrumento não é suficiente?" Encontre uma resposta no kit de bateria. Coordene uma coleção de tambores e pratos para ditar o ritmo da obra-prima musical.',
+      preço: '$649.00',
+      promoção: '$349.00'
     }
   ]
 };
