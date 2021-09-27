b7dfc130d5cfa8fc99d0e7912c59776ed9ed2ffa
V. S. Kanchev <27562113+inulo@users.noreply.github.com>
2021-09-27 18:36:04 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const context = {
   title: 'Welcome to Musicon',
-  body: 'Musicon is a budding musical storefront with a mission to share the joy of music. These magnificent auditory tools are designed with musical creators, like you, in mind. Hobbyists, beginners, and experts alike can appreciate the resplendent sounds supplied by each and every instrument we carry. Join us in delivering the euphoric vibrations of melodia to the citizens of the world!',
+  body: 'Musicon é uma loja musical em desenvolvimento com a missão de compartilhar a alegria da música. Essas magníficas ferramentas auditivas são projetadas com criadores musicais, como você, em mente. Hobbyists, iniciantes e especialistas podem apreciar os sons resplandecentes fornecidos por cada um dos instrumentos que carregamos. Junte-se a nós para entregar as vibrações eufóricas da melodia aos cidadãos do mundo!',
   instruments: [
     {
       image: 'https://content.codecademy.com/courses/learn-handlebars/musicon/electronic-keyboard.png',
